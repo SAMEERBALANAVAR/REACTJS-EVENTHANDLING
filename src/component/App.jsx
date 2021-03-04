f634adb8fe7aeb5fa8264059428ce3d67953679c
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-04 14:36:59 +0530

    ntg

---
+++ src/component/App.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <input type="text" placeholder="Enter your name" />
+      <button>Submit</button>
+    </div>
+  );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import react from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
-import App from "./App";
+import styles from "./styles.css";
+import App from "./component/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
