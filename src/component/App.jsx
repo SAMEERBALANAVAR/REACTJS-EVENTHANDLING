b53ed2de2ade437da9f7a4311936a15a018e0505
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-04 14:44:15 +0530

    ntng

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,11 +1,16 @@
-import react from "react";
+import react, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="Enter your name" />
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
